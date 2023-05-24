1318ea31a3bd0017cdf11141477c8ba4273923f4
Jotaro <zibble@mail.ru>
2023-05-24 17:51:51 +0400

    manager logic

--- src/main/java/com/hakathon/chatapi/rest/ChatController.java
+++ src/main/java/com/hakathon/chatapi/rest/ChatController.java
@@ -3,6 +3,7 @@ package com.hakathon.chatapi.rest;
 import com.hakathon.chatapi.UserStorage;
 import com.hakathon.chatapi.model.ChatEntity;
 import com.hakathon.chatapi.model.ChatRequest;
+import com.hakathon.chatapi.model.ReserveChatRequest;
 import com.hakathon.chatapi.repository.ChatRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -31,5 +32,23 @@ public class ChatController {
     @GetMapping("/userChats/{username}")
     public List<ChatEntity> getUserChats(@PathVariable String username) {
         return chatRepository.findAllByClientId(username);
+    }
+
+    @GetMapping("/avaibleChats/{username}")
+    public List<ChatEntity> getAvaibleChats(@PathVariable String username) {
+        List<ChatEntity> chats = chatRepository.findAll();
+        for (ChatEntity ce : chats) {
+            if(ce.getManagerId() != null)
+                if(ce.getManagerId() != username)
+                    chats.remove(ce);
+        }
+        return chats;
+    }
+
+    @PostMapping("/reserveChat")
+    public void reserveChat(@RequestBody ReserveChatRequest request) {
+        ChatEntity ce = chatRepository.findById(request.getChatId()).get();
+        ce.setManagerId(request.getManagerId());
+        chatRepository.save(ce);
     }
 }
